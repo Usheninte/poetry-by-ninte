a86a2f65d11389383dd88fbda214e1437cf073e6
Ninte <ninte.dev@gmail.com>
2020-11-01 11:45:04 +0100

    added social link

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,7 +24,8 @@ LINKS = (('Website', 'https://ninte.dev/'),
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/Usheninte'),
-          ('LinkedIn', 'https://www.linkedin.com/in/usheninte/'),)
+          ('LinkedIn', 'https://www.linkedin.com/in/usheninte/'),
+          ('GitHub', 'https://github.com/Usheninte'),)
 
 DEFAULT_PAGINATION = 42
 
@@ -32,13 +33,5 @@ DEFAULT_CATEGORY = 'Poetry'
 
 # Extra cofigurations
 
+STATIC_PATHS = ['images']
 RELATIVE_URLS = True
-LOAD_CONTENT_CACHE = False
-
-STATIC_PATHS = [
-    'extra',
-]
-
-EXTRA_PATH_METADATA = {
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-}
