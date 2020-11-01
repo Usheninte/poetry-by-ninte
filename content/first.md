bc3ad8b5ba399bd6ee1b3de9fd2876ef5c450bab
Ninte <ninte.dev@gmail.com>
2020-11-01 10:35:36 +0100

    Initial commit

---
+++ content/first.md
@@ -0,0 +1,8 @@
+Title: Beginnings
+Date: 2020-11-01 10:12
+Category: Poetry
+
+even as I waver  
+I know some things to be true  
+the rising of the sun, after night  
+and the face of the ocean, blue  
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Usheninte Dangana'
+SITENAME = 'Poetry by Ninte'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Africa/Lagos'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Website', 'https://ninte.dev/'),
+         ('Blog', 'https://blog.ninte.dev/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/Usheninte'),
+          ('LinkedIn', 'https://www.linkedin.com/in/usheninte/'),
+          ('Instagram', 'https://www.instagram.com/usheninte/'),)
+
+DEFAULT_PAGINATION = 42
+
+DEFAULT_CATEGORY = 'Poetry'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+LOAD_CONTENT_CACHE = False
